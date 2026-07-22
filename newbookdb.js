9c8f72c64c4bd90cc970c8102c0096c248a66fbd
Giovanni-Kristian-God-And-Me <ivankristian1st@gmail.com>
2026-07-22 15:42:07 +0700

    Add files via upload

---
+++ newbookdb.js
@@ -0,0 +1,29 @@
+// Database Buku Pelajaran
+
+var _bookDB = {
+  "神和我 God and Me": {
+    "Bab 1": {
+      "title": "Perkenalan & Gereja",
+      "vocab": [
+        {"char": "我", "pinyin": "wǒ", "id": "saya"},
+        {"char": "你", "pinyin": "nǐ", "id": "kamu"},
+        {"char": "好", "pinyin": "hǎo", "id": "baik"},
+        {"char": "叫", "pinyin": "jiào", "id": "dipanggil / bernama"},
+        {"char": "什么", "pinyin": "shén me", "id": "apa"},
+        {"char": "名字", "pinyin": "míng zi", "id": "nama"},
+        {"char": "请问", "pinyin": "qǐng wèn", "id": "permisi / tumpang tanya"},
+        {"char": "是", "pinyin": "shì", "id": "adalah / ya"},
+        {"char": "不", "pinyin": "bù", "id": "tidak / bukan"},
+        {"char": "吗", "pinyin": "ma", "id": "apakah (partikel tanya)"},
+        {"char": "呢", "pinyin": "ne", "id": "(partikel tanya balik / \"Bagaimana dengan...?\")"},
+        {"char": "欢迎", "pinyin": "huān yíng", "id": "selamat datang"},
+        {"char": "请", "pinyin": "qǐng", "id": "silakan / tolong"},
+        {"char": "进", "pinyin": "jìn", "id": "masuk"},
+        {"char": "平安", "pinyin": "píng ān", "id": "damai sejahtera"},
+        {"char": "弟兄", "pinyin": "dì xiōng", "id": "saudara seiman (laki-laki)"},
+        {"char": "牧师", "pinyin": "mù shī", "id": "pendeta"},
+        {"char": "教会", "pinyin": "jiào huì", "id": "gereja"}
+      ]
+    }
+  }
+};
